fa1e6515cb8cee1063dd58aede172d44fe7ee2cb
Mathieu Blondel <mathieu@mblondel.org>
2017-06-03 22:39:15 +0900

    Add setup files.

---
+++ setup.py
@@ -0,0 +1,65 @@
+from __future__ import print_function
+import os.path
+import sys
+import setuptools
+from numpy.distutils.core import setup
+
+
+try:
+    import numpy
+except ImportError:
+    print('numpy is required during installation')
+    sys.exit(1)
+
+
+DISTNAME = 'soft-dtw'
+DESCRIPTION = "Python implementation of soft-DTW"
+LONG_DESCRIPTION = open('README.rst').read()
+MAINTAINER = 'Mathieu Blondel'
+MAINTAINER_EMAIL = ''
+URL = 'https://github.com/mblondel/soft-dtw/'
+LICENSE = 'Simplified BSD'
+DOWNLOAD_URL = 'https://github.com/mblondel/soft-dtw/'
+VERSION = '0.1.dev0'
+
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration(None, parent_package, top_path)
+
+    config.add_subpackage('softdtw')
+
+    return config
+
+
+if __name__ == '__main__':
+    old_path = os.getcwd()
+    local_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+    os.chdir(local_path)
+    sys.path.insert(0, local_path)
+
+    setup(configuration=configuration,
+          name=DISTNAME,
+          maintainer=MAINTAINER,
+          include_package_data=True,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          license=LICENSE,
+          url=URL,
+          version=VERSION,
+          download_url=DOWNLOAD_URL,
+          long_description=LONG_DESCRIPTION,
+          zip_safe=False,  # the package can run out of an .egg file
+          classifiers=[
+              'Intended Audience :: Science/Research',
+              'Intended Audience :: Developers', 'License :: OSI Approved',
+              'Programming Language :: C', 'Programming Language :: Python',
+              'Topic :: Software Development',
+              'Topic :: Scientific/Engineering',
+              'Operating System :: Microsoft :: Windows',
+              'Operating System :: POSIX', 'Operating System :: Unix',
+              'Operating System :: MacOS'
+             ]
+          )
---
+++ softdtw/setup.py
@@ -0,0 +1,21 @@
+import os.path
+
+import numpy
+
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration('softdtw', parent_package, top_path)
+
+    config.add_extension('soft_dtw_fast', sources=['soft_dtw_fast.c'],
+                         include_dirs=[numpy.get_include()])
+
+    config.add_subpackage('tests')
+
+    return config
+
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
